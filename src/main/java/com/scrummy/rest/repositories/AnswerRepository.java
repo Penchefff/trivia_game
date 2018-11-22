23553d0de0f22a6f4dc186a985e68fb509026dfc
Svetoslav Georgiev <svetoslav.georgiev@sumup.com>
2018-11-22 15:56:45 +0200

    implement

--- src/main/java/com/scrummy/rest/repositories/AnswerRepository.java
+++ src/main/java/com/scrummy/rest/repositories/AnswerRepository.java
@@ -2,6 +2,8 @@ package com.scrummy.rest.repositories;
 
 import com.scrummy.rest.models.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
 }
