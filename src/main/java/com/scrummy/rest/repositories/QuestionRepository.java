f3567855dabb4252509bfb3feec212da6edcb297
Viktor <viktor.kostadinovski@sumup.com>
2018-11-22 11:54:59 +0200

    Implemented fetching of questions in random order with a limit of 10.

--- src/main/java/com/scrummy/rest/repositories/QuestionRepository.java
+++ src/main/java/com/scrummy/rest/repositories/QuestionRepository.java
@@ -2,6 +2,13 @@ package com.scrummy.rest.repositories;
 
 import com.scrummy.rest.models.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    @Query(nativeQuery = true,
+            value = "SELECT * FROM QUESTIONS ORDER BY RANDOM() LIMIT :limit")
+    List<Question> getQuestions(int limit);
 }
